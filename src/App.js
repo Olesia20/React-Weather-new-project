f0b55dfad2ccf08a456c2619020db612001d3cd3
olesia <infocooll.col@gmail.com>
2022-09-29 00:03:04 +0200

    Added links css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,42 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div className="container">
+      <div className="weather">
+        <div className="row">
+          <div className="col-6">
+            <ul>
+              <li>Friday: 22:20</li>
+              <li>Sunny</li>
+              <li>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1779/1779940.png"
+                  width={80}
+                  alt="Clouds"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Feels like: 30°C</li>
+              <li>Humidity:30%</li>
+              <li>Wind: 3 km/h</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-6">
+            <h1>Kyiv</h1>
+          </div>
+
+          <div className="col-6">
+            <h1>30°C</h1>
+          </div>
+        </div>
+        <hr />
+      </div>
     </div>
   );
 }
